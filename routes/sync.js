4a3cfb4160d640b9a1558583df0e9194c5f3e605
K <chesnokovks@gmail.com>
2020-03-25 19:01:30 +0300

    sync progress

--- routes/sync.js
+++ routes/sync.js
@@ -35,93 +35,117 @@ router.post('/', [auth, authSpotify], async (req, res) => {
       params: { limit: 50, after: history.date ? history.date : 0 }
     };
     const spRes = await axios(options);
-    const timestamp = spRes.data.cursors.after;
-    const historyTracks = spRes.data.items.map(e => {
-      return { spID: e.track.id, name: e.track.name, playedAt: e.played_at };
-    });
+    const timestamp = spRes.data.cursors ? spRes.data.cursors.after : null;
+    if (timestamp) {
+      const historyTracks = spRes.data.items.map(e => {
+        return { spID: e.track.id, name: e.track.name, playedAt: e.played_at };
+      });
 
-    const trackedTracks = await Artist.aggregate([
-      {
-        $match: {
-          user: userID,
-          isTracked: true,
-          isArchived: false
-        }
-      },
-      {
-        $project: {
-          name: 1,
-          albums: 1,
-          albums: {
-            $filter: {
-              input: '$albums',
-              as: 'album',
-              cond: {
-                $and: ['$$album.isTracked']
+      const trackedTracks = await Artist.aggregate([
+        {
+          $match: {
+            user: userID,
+            isTracked: true,
+            isArchived: false
+          }
+        },
+        {
+          $project: {
+            name: 1,
+            albums: 1,
+            albums: {
+              $filter: {
+                input: '$albums',
+                as: 'album',
+                cond: {
+                  $and: ['$$album.isTracked']
+                }
               }
             }
           }
-        }
-      },
-      { $unwind: '$albums' },
-      {
-        $project: {
-          name: 1,
-          tracks: 1,
-          'albums.name': 1,
-          'albums._id': 1,
-          tracks: {
-            $filter: {
-              input: '$albums.tracks',
-              as: 'track',
-              cond: {
-                $and: ['$$track.isTracked', { $lte: ['$$track.listens', 100] }]
+        },
+        { $unwind: '$albums' },
+        {
+          $project: {
+            name: 1,
+            tracks: 1,
+            'albums.name': 1,
+            'albums._id': 1,
+            tracks: {
+              $filter: {
+                input: '$albums.tracks',
+                as: 'track',
+                cond: {
+                  $and: [
+                    '$$track.isTracked',
+                    { $lte: ['$$track.listens', 100] }
+                  ]
+                }
               }
             }
           }
-        }
-      },
-      { $unwind: '$tracks' }
-    ]);
-    const updatedTracks = trackedTracks
-      .filter(trackedTrack =>
-        historyTracks.some(
-          historyTrack => trackedTrack.tracks.spID === historyTrack.spID
+        },
+        { $unwind: '$tracks' }
+      ]);
+      const updatedTracks = trackedTracks
+        .filter(trackedTrack =>
+          historyTracks.some(
+            historyTrack => trackedTrack.tracks.spID === historyTrack.spID
+          )
         )
-      )
-      .map(trackedTrack => {
-        return {
-          ...trackedTrack,
-          tracks: {
-            ...trackedTrack.tracks,
-            lastListen: historyTracks.find(
-              historyTrack => historyTrack.spID === trackedTrack.tracks.spID
-            ).playedAt,
-            listens:
-              trackedTrack.tracks.listens +
-              historyTracks.filter(
+        .map(trackedTrack => {
+          return {
+            ...trackedTrack,
+            tracks: {
+              ...trackedTrack.tracks,
+              lastListen: historyTracks.find(
                 historyTrack => historyTrack.spID === trackedTrack.tracks.spID
-              ).length
+              ).playedAt,
+              listens:
+                trackedTrack.tracks.listens +
+                historyTracks.filter(
+                  historyTrack => historyTrack.spID === trackedTrack.tracks.spID
+                ).length
+            }
+          };
+        })
+        .sort(
+          (a, b) =>
+            new Date(b.tracks.lastListen).getTime() -
+            new Date(a.tracks.lastListen).getTime()
+        );
+
+      const generateUpdate = trackToUpdate => {
+        return {
+          updateOne: {
+            filter: {
+              _id: trackToUpdate._id
+            },
+            update: {
+              $set: {
+                'albums.$[album].tracks.$[track].listens':
+                  trackToUpdate.tracks.listens
+              }
+            },
+            arrayFilters: [
+              { 'album._id': trackToUpdate.albums._id },
+              { 'track._id': trackToUpdate.tracks._id }
+            ]
           }
         };
-      });
-    const generateUpdate = trackToUpdate => {
-      return {
-        updateOne: {
-          filter: {
-            _id: trackToUpdate._id
-          },
-          update: { $set: { 'album.$[album].tracks.$[track].listens': 100 } },
-          arrayFilters: [
-            { 'album._id': trackToUpdate.albums._id },
-            { 'track._id': trackToUpdate.tracks._id }
-          ]
-        }
       };
-    };
-    const update = updatedTracks.map(generateUpdate);
-    await Artist.bulkWrite(update);
-    res.json(update);
+      const update = updatedTracks.map(generateUpdate);
+      await Artist.bulkWrite(update);
+      history.tracks.unshift(...updatedTracks);
+      if (history.tracks.length > 50) {
+        history.tracks.splice(50, history.tracks.length - 50);
+      }
+      history.date = timestamp;
+      await history.save();
+      res.json(updatedTracks);
+    } else {
+      res.json({ msg: 'No history changes since last sync' });
+    }
   } catch (err) {
     const status = err.response ? err.response.status : 500;
     const msg = err.response
