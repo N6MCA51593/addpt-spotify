3b6db92fbc536e5df3e6e51eef8149481445ad4a
K <chesnokovks@gmail.com>
2020-04-07 23:52:12 +0300

    useApiRequest progress

---
+++ client/src/utils/useAPIRequest.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+const types = {
+  REQUEST_INIT: 'REQUEST_INIT',
+  REQUEST_SUCCESS: 'REQUEST_SUCCESS',
+  REQUEST_FAIL: 'REQUEST_FAIL'
+};
+
+const hookStateReducer = (state, action) => {};
+
+const useAPIRequest = () => {
+  return <div></div>;
+};
+
+export default useAPIRequest;
--- client/src/utils/useServerRequest.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const useServerRequest = () => {
-  return <div></div>;
-};
