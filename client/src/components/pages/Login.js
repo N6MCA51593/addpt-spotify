f8ab49bfa108bee92764a5ff003bbde19c9f7b6c
K <chesnokovks@gmail.com>
2020-06-06 23:42:29 +0300

    landing page wip

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import AuthContext from '../../context/auth/authContext';
 import AlertContext from '../../context/alert/alertContext';
+import image from '../../assets/landing_page_image.png';
 
 const Login = props => {
   const { isAuthenticated, error, clearErrors } = useContext(AuthContext);
@@ -17,11 +18,25 @@ const Login = props => {
     }
   }, [isAuthenticated, props.history, error, setAlert, clearErrors]);
   return (
-    <div>
-      Please Log In
-      <form action='/api/auth' method='post'>
-        <input type='submit' value='Submit' />
-      </form>
+    <div className='landing'>
+      <div className='main'>
+        <div className='text'>
+          <h1>
+            Every artist you meet on your musical journey presents a voyage of
+            their own, so it helps to have a <span>MAP</span>
+          </h1>
+          <h3>Artist Discography Discovery Progress Tracker for Spotify </h3>
+          <form action='/api/auth' method='post'>
+            <button type='submit'>Sign in with Spotify</button>
+          </form>
+        </div>
+        <div className='image-container'>
+          <div>
+            <img src={image} alt='App' />
+          </div>
+        </div>
+      </div>
+      <div className='secondary'></div>
     </div>
   );
 };
