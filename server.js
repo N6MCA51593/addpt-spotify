ad272392afe4fe618741159ea1fc795cd411885a
K <chesnokovks@gmail.com>
2020-06-11 00:06:05 +0300

    1

--- server.js
+++ server.js
@@ -36,9 +36,10 @@ setInterval(async () => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.get('*', (req, res) => {
+    console.log('object');
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
