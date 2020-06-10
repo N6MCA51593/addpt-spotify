105c504d3d0f51279720ed6fe7436f0ffdfe0f5d
K <chesnokovks@gmail.com>
2020-06-10 23:03:01 +0300

    deploy

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ setInterval(async () => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
